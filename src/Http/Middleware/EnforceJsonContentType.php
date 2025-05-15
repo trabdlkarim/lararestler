5d2a0aafee96ddb0c7ca278f96594f4f34188f3e
Abdoul Karim Toure <trabdlkarim@zohomail.eu>
2025-05-15 21:59:49 +0200

    Add middleware to enforce json content type

---
+++ src/Http/Middleware/EnforceJsonContentType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Mirak\Lararestler\Http\Middleware;
+
+use Mirak\Lararestler\Exceptions\RestException;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnforceJsonContentType
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!in_array($request->method(), ['GET', 'HEAD', 'OPTIONS']) && !$request->expectsJson()) {
+            $message = "Content-Type header is missing.";
+            if ($request->hasHeader('Content-Type')) {
+                $message = 'Content type `' . $request->header('Content-Type') . '` is not supported.';
+            }
+            throw new RestException(406,  $message);
+        }
+        return $next($request);
+    }
+}
